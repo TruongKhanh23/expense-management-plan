7b7853b79ad76aa2eddf65e0efb2f24bac932573
TruongKhanh23 <138382115+TruongKhanh23@users.noreply.github.com>
2023-10-01 19:09:00 +0700

    Feat/integrate from portfolio (#5)
    * chore: config alias, instal ant design vue, vue splide js
    
    * chore: move all expense management plan from portfolio
    
    * chore: install node
    
    * chore: testing deploy vercel

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,333 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { onMounted } from "vue";
-
-onMounted(() => {
-  const script = document.createElement("script");
-
-  script.src = "https://buttons.github.io/buttons.js";
-  script.async = true;
-
-  document.body.appendChild(script);
-});
-</script>
-
 <template>
-  <div class="bg-gray-800 h-screen text-white">
-    <header class="py-16">
-      <h1 class="text-5xl font-bold text-center mb-6">Create Tailwind</h1>
-      <p className="text-center mb-6 text-xl">
-        If you like this project, consider giving it a star on GitHub!
-      </p>
+  <div class="container mx-auto mt-12">
+    <Funds :funds="funds" :totalIncome="totalIncome" />
 
-      <div class="flex flex-row justify-center items-center gap-4">
-        <a
-          class="github-button"
-          href="https://github.com/andrejjurkin/create-tw"
-          data-color-scheme="no-preference: dark; light: dark; dark: dark;"
-          data-icon="octicon-star"
-          data-size="large"
-          data-show-count="true"
-          aria-label="Star andrejjurkin/create-tw on GitHub"
-        >
-          Star
-        </a>
-        <a
-          class="github-button"
-          href="https://github.com/andrejjurkin/create-tw/discussions"
-          data-color-scheme="no-preference: dark; light: dark; dark: dark;"
-          data-icon="octicon-comment-discussion"
-          data-size="large"
-          aria-label="Discuss andrejjurkin/create-tw on GitHub"
-        >
-          Discuss
-        </a>
-      </div>
-    </header>
+    <!-- Mobile View -->
+    <div class="md:hidden">
+      <a-tabs v-model:activeKey="activeKey" centered>
+        <a-tab-pane key="1" tab="Dự chi thiết yếu">
+          <EstimateNecessity :necessityLimitation="necessityLimitation" />
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="Thu nhập" force-render>
+          <IncomeDebt
+            class="md:border-l md:border-r px-4"
+            :columns="columnsIncome"
+            :data="dataIncome"
+            :totalIncome="totalIncome"
+          />
+        </a-tab-pane>
+        <a-tab-pane key="3" tab="Xử lý thu nhập">
+          <HandleIncome
+            :columns="columnsHandleIncome"
+            :data="dataHandleIncome"
+          />
+        </a-tab-pane>
+      </a-tabs>
+    </div>
 
-    <div class="flex gap-16 items-center justify-center">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="w-24 h-24" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="w-24 h-24" alt="Vue logo" />
-      </a>
+    <!-- Desktop View-->
+    <div class="flex flex-col md:flex-row hidden md:flex">
+      <a-col :md="{ span: 6 }">
+        <EstimateNecessity :necessityLimitation="necessityLimitation" />
+      </a-col>
+
+      <a-col :md="{ span: 8 }">
+        <IncomeDebt
+          class="md:border-l md:border-r px-4"
+          :columns="columnsIncome"
+          :data="dataIncome"
+          :totalIncome="totalIncome"
+        />
+      </a-col>
+
+      <a-col :md="{ span: 10 }">
+        <HandleIncome :columns="columnsHandleIncome" :data="dataHandleIncome" />
+      </a-col>
     </div>
   </div>
 </template>
+<script>
+import { Col, Tabs, TabPane } from "ant-design-vue";
+import Funds from "./components/Funds.vue";
+import IncomeDebt from "./components/IncomeDebt.vue";
+import HandleIncome from "./components/HandleIncome.vue";
+import EstimateNecessity from "./components/EstimateNecessity.vue";
+
+export default {
+  components: {
+    ACol: Col,
+    ATabs: Tabs,
+    ATabPane: TabPane,
+    Funds,
+    IncomeDebt,
+    HandleIncome,
+    EstimateNecessity,
+  },
+  setup() {
+    const columnsIncome = [
+      {
+        title: "Nguồn thu nhập",
+        dataIndex: "source",
+        key: "source",
+        width: 200,
+      },
+      {
+        title: "Số tiền",
+        dataIndex: "amount",
+        key: "amount",
+      },
+    ];
+    const dataIncome = [
+      {
+        key: "1",
+        source: "Lương",
+        amount: 13500000,
+      },
+      {
+        key: "2",
+        source: "Ba cho",
+        amount: 2000000,
+      },
+      {
+        key: "3",
+        source: "Mẹ cho",
+        amount: 4500000,
+      },
+    ];
+    const columnsHandleIncome = [
+      {
+        title: "Loại",
+        dataIndex: "type",
+        maxWidth: 50,
+      },
+      {
+        title: "Quỹ",
+        dataIndex: "fund",
+      },
+      {
+        title: "Số tiền",
+        dataIndex: "amount",
+      },
+    ];
+
+    const dataHandleIncome = [
+      {
+        key: "1",
+        wallet: "Momo",
+        fund: "Q. CT thiết yếu",
+        type: "necessity",
+        amount: 1259499,
+      },
+      {
+        key: "2",
+        wallet: "Momo",
+        fund: "Q. Nội thất",
+        type: "necessity",
+        amount: 200000,
+      },
+      {
+        key: "3",
+        wallet: "VinID",
+        fund: "Q. Du lịch",
+        type: "freedom",
+        amount: 600000,
+      },
+      {
+        key: "4",
+        wallet: "Momo",
+        fund: "Ví chính H.Thụ",
+        type: "enjoy",
+        amount: 670000,
+      },
+      {
+        key: "5",
+        wallet: "Tiền mặt",
+        fund: "Hưởng thụ",
+        type: "enjoy",
+        amount: 400000,
+      },
+      {
+        key: "6",
+        wallet: "Momo",
+        fund: "Chuyển tiền ăn vặt vợ",
+        type: "enjoy",
+        amount: 300000,
+      },
+      {
+        key: "7",
+        wallet: "VinID",
+        fund: "Q. Gia hạn sim",
+        type: "enjoy",
+        amount: 100000,
+      },
+      {
+        key: "8",
+        wallet: "VinID",
+        fund: "Qũy Vieon",
+        type: "enjoy",
+        amount: 30000,
+      },
+      {
+        key: "9",
+        wallet: "VNPay",
+        fund: "Q. Học tập",
+        type: "education",
+        amount: 265000,
+      },
+      {
+        key: "10",
+        wallet: "VinID",
+        fund: "Qũy Goalify",
+        type: "education",
+        amount: 35000,
+      },
+      {
+        key: "11",
+        wallet: "VinID",
+        fund: "Trả mua ghế công thái",
+        type: "education",
+        amount: 100000,
+      },
+      {
+        key: "12",
+        wallet: "ZaloPay",
+        fund: "Trả tiền lễ tốt nghiệp",
+        type: "education",
+        amount: 200000,
+      },
+      {
+        key: "13",
+        wallet: "ZaloPay",
+        fund: "Q. Cho đi",
+        type: "giving",
+        amount: NaN,
+      },
+      {
+        key: "14",
+        wallet: "VinID",
+        fund: "Qũy birthday mẹ Sang",
+        type: "giving",
+        amount: 100000,
+      },
+      {
+        key: "15",
+        wallet: "VinID",
+        fund: "Qũy birthday mẹ Linh",
+        type: "giving",
+        amount: 100000,
+      },
+      {
+        key: "16",
+        wallet: "ZaloPay",
+        fund: "Trả tiền cưới Thương Q",
+        type: "giving",
+        amount: 400000,
+      },
+      {
+        key: "17",
+        wallet: "ZaloPay",
+        fund: "TK tích lũy",
+        type: "longTermSaving",
+        amount: 2200000,
+      },
+      {
+        key: "18",
+        wallet: "VinID",
+        fund: "Q. Lavie",
+        type: "necessity",
+        amount: 222000,
+      },
+      {
+        key: "19",
+        wallet: "Shopee",
+        fund: "Q. Khám bệnh",
+        type: "necessity",
+        amount: 300000,
+      },
+    ];
+    const totalIncome = calculateTotalAmount(dataIncome);
+
+    function calculateTotalAmount(data) {
+      let totalAmount = 0;
+
+      for (const element of data) {
+        totalAmount += element.amount;
+      }
+
+      return totalAmount;
+    }
+    const funds = [
+      {
+        id: "",
+        src: "",
+        percentage: "",
+        wallet: "",
+        name: "",
+        classColor: "",
+      },
+      {
+        id: "necessity",
+        src: "../src/assets/images/momo.svg",
+        percentage: 72.5,
+        wallet: "Túi thần tài",
+        name: "Nhu cầu thiết yếu",
+        classColor: "bg-[#ffabab]",
+      },
+      {
+        id: "freedom",
+        src: "../src/assets/images/momo.svg",
+        percentage: 3,
+        wallet: "Qũy nhóm",
+        name: "Tự do tài chính",
+        classColor: "bg-[#63b5ff]",
+      },
+      {
+        id: "education",
+        src: "../src/assets/images/vnpay.png",
+        percentage: 3,
+        wallet: "Ví chính",
+        name: "Giáo dục đào tạo",
+        classColor: "bg-[#97a2ff]",
+      },
+      {
+        id: "relax",
+        src: "../src/assets/images/momo.svg",
+        percentage: 7.5,
+        wallet: "Ví chính",
+        name: "Hưởng thụ",
+        classColor: "bg-[#76de82]",
+      },
+      {
+        id: "giving",
+        src: "../src/assets/images/zalopay.png",
+        percentage: 3,
+        wallet: "Ví chính",
+        name: "Cho đi",
+        classColor: "bg-[#c0c0c0]",
+      },
+      {
+        id: "longTermSaving",
+        src: "../src/assets/images/zalopay.png",
+        percentage: 11,
+        wallet: "Tài khoản tích lũy",
+        name: "Tiết kiệm dài hạn",
+        classColor: "bg-[#f9a484]",
+      },
+    ];
+
+    const necessityLimitation =
+      (funds.find((item) => item.id === "necessity").percentage * totalIncome) /
+      100;
+    console.log("necessityLimitation", necessityLimitation);
+    return {
+      columnsIncome,
+      dataIncome,
+      funds,
+      totalIncome,
+      columnsHandleIncome,
+      dataHandleIncome,
+      necessityLimitation,
+    };
+  },
+};
+</script>
