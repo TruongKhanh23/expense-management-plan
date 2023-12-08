20c11bc2b00f6f6ad74041118b146f2f88ec4904
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2023-12-08 14:02:04 +0700

    feat: create common buildPathSegments

---
+++ src/composables/segment/index.js
@@ -0,0 +1,29 @@
+export function buildPathSegments(
+  object,
+  year = "2023",
+  monthYear = "01-2023",
+  user = "admin",
+) {
+  const commonPathSegments = [
+    "users",
+    "user",
+    "years",
+    "year",
+    "months",
+    "monthYear",
+  ];
+  commonPathSegments.forEach((item, index, array) => {
+    if (item === "user") {
+      array[index] = user;
+    }
+    if (item === "year") {
+      array[index] = year;
+    }
+    if (item === "monthYear") {
+      array[index] = monthYear;
+    }
+  });
+  commonPathSegments.push(object);
+  console.log("commonPathSegments", ...commonPathSegments);
+  return commonPathSegments;
+}
