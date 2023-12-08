b9b9881735a6c7dd58a48667d75e535df59b38c4
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2023-12-08 14:02:33 +0700

    feat: function createNewMonth

---
+++ src/composables/collection/index.js
@@ -0,0 +1,157 @@
+import { ref } from "vue";
+import { db } from "@/main";
+import dayjs from "dayjs";
+import sampleJson from "@/assets/data/sample.json";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  setDoc,
+  doc,
+  getDocs,
+} from "firebase/firestore";
+
+const pathSegments = [
+  "users",
+  "admin",
+  "years",
+  "2023",
+  "months",
+  "01-2023",
+  "handleIncomes",
+];
+
+const countries = ref([]);
+export async function createNewMonth() {
+  let currentDate = new Date();
+  let currentYear = currentDate.getFullYear().toString();
+  let currentMonth = (currentDate.getMonth() + 1).toString();
+  let lastMonth = (currentMonth - 1).toString();
+
+  console.log("lastMonth", lastMonth);
+
+  let currentMonthYear = `${currentMonth}-${currentYear}`;
+
+  console.log("currentMonth", currentMonth);
+
+  const pathSegments = ["users", "admin", "years", "2023", "months"];
+
+  // Create document month-year (Ex: 02-2023)
+  let dataObject = { name: `Tháng ${currentMonth}, năm ${currentYear}` };
+  // doc(firestoreInstance, "collectionName", "documentId")
+  // setDoc(collectionReference, dataObject)
+  try {
+    await setDoc(doc(db, ...pathSegments, currentMonthYear), dataObject);
+    console.log(`Set month ${dataObject.name} successfully`);
+    console.log("--------------------------------------------------------------------------------------");
+  } catch (error) {
+    console.log("Failed to set month");
+  }
+
+  //Create collection funds/education
+  const objects = [
+    "estimateNecessityExpenses",
+    "funds",
+    "handleIncomes",
+    "incomes",
+  ];
+  objects.forEach((object) => {
+    switch (object) {
+      case "estimateNecessityExpenses": {
+        const data = sampleJson.estimateNecessityExpenses;
+        const estimateNecessityExpenses = [
+          "healthFund",
+          "necessity",
+          "necessityFund",
+          "waterLavieFund",
+        ];
+
+        const newPathSegments = [...pathSegments, currentMonthYear, object];
+        estimateNecessityExpenses.forEach(async (documentId) => {
+          const dataObject = data.find((item) => item.id === documentId);
+          try {
+            await setDoc(doc(db, ...newPathSegments, documentId), dataObject);
+            console.log(`Set data document ${dataObject.id} successfully`);
+          } catch (error) {
+            console.log("Failed to set data document ${dataObject.id}");
+          }
+        });
+        console.log("Set estimateNecessityExpenses successfully");
+        console.log("--------------------------------------------------------------------------------------");
+
+        break;
+      }
+      case "funds": {
+        const data = sampleJson.funds;
+        const funds = [
+          "education",
+          "freedom",
+          "giving",
+          "longTermSaving",
+          "necessity",
+          "relax",
+        ];
+
+        const newPathSegments = [...pathSegments, currentMonthYear, object];
+        funds.forEach(async (documentId) => {
+          const dataObject = data.find((item) => item.id === documentId);
+          try {
+            await setDoc(doc(db, ...newPathSegments, documentId), dataObject);
+            console.log(`Set data document ${dataObject.id} successfully`);
+          } catch (error) {
+            console.log(`Failed to set data document ${dataObject.id}`);
+          }
+        });
+        console.log("Set funds successfully");
+        console.log("--------------------------------------------------------------------------------------");
+        break;
+      }
+      case "handleIncomes": {
+        const data = sampleJson.handleIncomes;
+        const handleIncomes = [
+          "education",
+          "freedom",
+          "giving",
+          "longTermSaving",
+          "necessity",
+          "relax",
+        ];
+
+        const newPathSegments = [...pathSegments, currentMonthYear, object];
+        handleIncomes.forEach(async (documentId) => {
+          const dataObject = data.find((item) => item.id === documentId);
+          try {
+            await setDoc(doc(db, ...newPathSegments, documentId), dataObject);
+            console.log(`Set data document ${dataObject.id} successfully`);
+          } catch (error) {
+            console.log(`Failed to set data document ${dataObject.id}`);
+          }
+        });
+        console.log("Set handleIncomes successfully");
+        console.log("--------------------------------------------------------------------------------------");
+        break;
+      }
+      case "incomes": {
+        const data = sampleJson.incomes;
+        const handleIncomes = ["1", "2", "3"];
+
+        const newPathSegments = [...pathSegments, currentMonthYear, object];
+        handleIncomes.forEach(async (documentId) => {
+          const dataObject = data.find((item) => item.key === documentId);
+          try {
+            await setDoc(doc(db, ...newPathSegments, documentId), dataObject);
+            console.log(`Set data document ${dataObject.key} successfully`);
+          } catch (error) {
+            console.log(`Failed to set data document ${dataObject.key}`);
+          }
+        });
+        console.log("Set incomes successfully");
+        console.log("--------------------------------------------------------------------------------------");
+        break;
+      }
+      default:
+        break;
+    }
+  });
+}
