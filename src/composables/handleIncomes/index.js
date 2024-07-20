a7ce4603e98d4fc4828e4079c7416fae1dfadbcb
TruongKhanh23 <truongnguyenkhanh230800@gmail.com>
2024-07-20 09:39:08 +0700

    EMP-004: Fix set get handle incomes

--- src/composables/handleIncomes/index.js
+++ src/composables/handleIncomes/index.js
@@ -1,31 +1,34 @@
 import { ref } from "vue";
 import { db } from "@/main";
-import { buildPathSegments } from "@/composables/segment/index.js"
+import { buildPathSegments } from "@/composables/segment/index.js";
 import { collection, onSnapshot, query, setDoc, doc } from "firebase/firestore";
+import { getCurrentChooseMonth } from "@/utils/time.util";
 
 export async function getHandleIncomes(year, monthYear, user = "admin") {
   try {
     const count = ref(0);
     const list = ref([]);
-    const pathSegments = buildPathSegments("handleIncomes", year, monthYear, user)
-    onSnapshot(
-      query(collection(db, ...pathSegments)),
-      (snap) => {
-        count.value++;
-        if (count.value > 1) {
-          list.value = [];
-        }
-        snap.forEach((doc) => {
-          const data = doc.data();
-          const item = {
-            key: doc.id,
-            ...data,
-          };
-          list.value.push(item);
-        });
-        localStorage.setItem("handleIncomes", JSON.stringify(list.value));
-      },
+    const pathSegments = buildPathSegments(
+      "handleIncomes",
+      year,
+      monthYear,
+      user,
     );
+    onSnapshot(query(collection(db, ...pathSegments)), (snap) => {
+      count.value++;
+      if (count.value > 1) {
+        list.value = [];
+      }
+      snap.forEach((doc) => {
+        const data = doc.data();
+        const item = {
+          key: doc.id,
+          ...data,
+        };
+        list.value.push(item);
+      });
+      localStorage.setItem("handleIncomes", JSON.stringify(list.value));
+    });
     return list.value;
   } catch (error) {
     alert("Get handleIncomes failed");
@@ -35,7 +38,9 @@ export async function getHandleIncomes(year, monthYear, user = "admin") {
 export async function setHandleIncomes(values) {
   try {
     const id = values[0].type;
-    const pathSegments = buildPathSegments("handleIncomes")
+    const year = getCurrentChooseMonth().year;
+    const monthYear = getCurrentChooseMonth().monthYear;
+    const pathSegments = buildPathSegments("handleIncomes", year, monthYear);
     await setDoc(
       doc(db, ...pathSegments, id),
       {
