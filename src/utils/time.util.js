67ca64a94be810db122909356abd0ecf4213d7df
TruongKhanh23 <truongnguyenkhanh230800@gmail.com>
2024-07-20 08:45:12 +0700

    EMP-004: Add util setCurrentChooseMonth, getCurrentChooseMonth

--- src/utils/time.util.js
+++ src/utils/time.util.js
@@ -1,8 +1,28 @@
 export function getCurrentTime() {
   const currentDate = new Date();
-  const currentMonth = currentDate.getMonth() < 10 ? "0" + (currentDate.getMonth() + 1).toString() : (currentDate.getMonth() + 1).toString();
+  const currentMonth =
+    currentDate.getMonth() < 10
+      ? "0" + (currentDate.getMonth() + 1).toString()
+      : (currentDate.getMonth() + 1).toString();
   const currentYear = currentDate.getFullYear().toString();
   const currentMonthYear = `${currentMonth}-${currentYear}`;
 
   return { currentYear, currentMonth, currentMonthYear };
+}
+
+export function setCurrentChooseMonth(year, monthYear) {
+  localStorage.setItem(
+    "currentChooseMonth",
+    JSON.stringify({ year, monthYear }),
+  );
+}
+
+export function getCurrentChooseMonth() {
+  const currentChooseMonth = JSON.parse(
+    localStorage.getItem("currentChooseMonth"),
+  );
+  return {
+    year: currentChooseMonth.year,
+    monthYear: currentChooseMonth.monthYear,
+  };
 }
