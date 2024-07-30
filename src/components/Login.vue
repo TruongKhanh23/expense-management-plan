b38312ebcb7d2d1b31b7398f032140d577604d13
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2024-07-30 15:10:51 +0700

    EMP-019: Refactor Class CSS Login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-center items-center min-h-screen bg-white">
-    <div class="w-full max-w-sm bg-white dark:bg-[#181A1B] p-4 sm:p-6 md:p-8 md:border md:border-gray-200 dark:border-gray-700 rounded-lg sm:shadow">
+    <div class="w-full max-w-sm bg-white dark:bg-[#181A1B] p-6 md:border md:border-gray-200 dark:border-gray-700 rounded-lg sm:shadow">
       <LoginForm @action:openResetPasswordModal="handleOpenResetPasswordModal" />
       <SignInWithGoogle />
     </div>
