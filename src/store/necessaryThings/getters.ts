46d2505cebc1ba5394d4f88176380a491f1853af
Trương Khánh <truongn.khanh@dai-ichi-life.com.vn>
2024-09-24 18:04:14 +0700

    EMP-054: Total Per Month Necessary Things

--- src/store/necessaryThings/getters.ts
+++ src/store/necessaryThings/getters.ts
@@ -1,9 +1,18 @@
 import { State } from "@/store/state";
+import { roundDecimals } from "@/utils/number.util";
 import type { NecessaryThingsItem, NecessaryThingsType } from "@/types/types";
 
 export const getters = {
   getNecessaryThings(state: State): NecessaryThingsItem[] {
     return state.necessaryThings;
+  },
+  getTotalMonthNecessaryThings(state: State): string {
+    let totalPerMonth = 0;
+    for(const item of state.necessaryThings) {
+      totalPerMonth += item.savePerMonth;
+    }
+    const result = roundDecimals(totalPerMonth);
+    return result;
   },
   getNecessaryThingsType(state: State): NecessaryThingsType[] {
     return state.necessaryThingsType;
