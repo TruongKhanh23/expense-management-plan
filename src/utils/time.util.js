8f883e26034a81c5f3de8df7fa2d82ef4ff9907b
Truong Khanh <truongnguyenkhanh230800@gmail.com>
2024-09-28 10:48:04 +0700

    EMP-059: Update hàm getCurrentTime

--- src/utils/time.util.js
+++ src/utils/time.util.js
@@ -2,17 +2,37 @@ import store from "@/store";
 
 export function getCurrentTime() {
   const currentDate = new Date();
+
+  // Lấy ngày 26 theo UTC
+  let payDay = new Date(
+    Date.UTC(currentDate.getUTCFullYear(), currentDate.getUTCMonth(), 26),
+  );
+
+  // Điều chỉnh ngày trả lương nếu ngày 26 là thứ 7 hoặc Chủ nhật
+  const dayOfWeek = payDay.getUTCDay();
+  if (dayOfWeek === 6) {
+    // Thứ 7
+    payDay.setUTCDate(25);
+  } else if (dayOfWeek === 0) {
+    // Chủ nhật
+    payDay.setUTCDate(24);
+  }
+
+  // Xác định tháng hiện tại dựa trên ngày trả lương
+  let currentMonth = payDay.getUTCMonth() + 2; // Thêm 1 ở đây
+  if (currentDate < payDay) {
+    currentMonth = currentMonth - 1 === 0 ? 12 : currentMonth - 1;
+  }
+
+  const currentMonthString =
+    currentMonth < 10 ? "0" + currentMonth : currentMonth.toString();
+  const currentYear = currentDate.getUTCFullYear().toString();
+  const currentMonthYear = `${currentMonthString}-${currentYear}`;
   const currentDateString = currentDate.toString();
-  const currentMonth =
-    currentDate.getMonth() < 10
-      ? "0" + (currentDate.getMonth() + 1).toString()
-      : (currentDate.getMonth() + 1).toString();
-  const currentYear = currentDate.getFullYear().toString();
-  const currentMonthYear = `${currentMonth}-${currentYear}`;
 
   return {
     currentYear,
-    currentMonth,
+    currentMonth: currentMonthString,
     currentMonthYear,
     currentDate: currentDateString,
   };
